7bf4729d716eeed630e15ca733eba1c6b64c7ae7
juh9870 <juh9870@ukr.net>
2024-04-06 21:40:35 +0300

    fix(egui-tweak): proper nested grouping

--- egui-tweak/src/editor.rs
+++ egui-tweak/src/editor.rs
@@ -4,33 +4,33 @@ use std::iter::Peekable;
 
 pub fn edit_tweakables(ui: &mut Ui) {
     let mut tweakables = crate::get_all_tweakables();
-    ui.vertical(|ui| edit_items(ui, "", &mut tweakables.iter_mut().peekable()));
+    ui.vertical(|ui| edit_items(ui, &[], &mut tweakables.iter_mut().peekable()));
 }
 
 /// Recursively draw tree of tweekable items
 fn edit_items<'a, Iter: Iterator<Item = (&'a Name, &'a mut Box<dyn Tweakable>)>>(
     ui: &mut Ui,
-    prefix: &str,
+    prefix: &[&str],
     items: &mut Peekable<Iter>,
 ) {
     while let Some((path, _)) = items.peek() {
-        if !path.starts_with(prefix) {
+        let mut segments: Vec<&str> = path.split('.').collect();
+        if !segments.starts_with(prefix) {
             return;
         }
-        let mut segments: Vec<&str> = path.split('.').collect();
         let name = segments
             .pop()
             .expect("Tweakable paths should not be empty")
             .to_string();
-        let new_prefix = segments.join(".");
-        if prefix != new_prefix {
-            let collapsed = new_prefix.trim_start_matches(prefix);
-            ui.collapsing(collapsed, |ui| {
-                edit_items(ui, &new_prefix, items);
+        if prefix.len() != segments.len() {
+            let next_level = segments[..(prefix.len() + 1)].to_vec();
+            let next_level_str = next_level.join(".");
+            ui.collapsing(next_level.last().unwrap().to_string(), |ui| {
+                edit_items(ui, &next_level, items);
             });
             while items
                 .peek()
-                .is_some_and(|(name, _)| name.starts_with(&new_prefix))
+                .is_some_and(|(name, _)| name.starts_with(&next_level_str))
             {
                 items.next();
             }
